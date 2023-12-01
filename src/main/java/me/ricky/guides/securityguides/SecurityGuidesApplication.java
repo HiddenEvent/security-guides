9447eef37b4f022d487073d075e4f8ee2d831f04
RichardKim <hiddeneventor@gmail.com>
2023-12-01 23:26:48 +0900

    Initial commit

---
+++ src/main/java/me/ricky/guides/securityguides/SecurityGuidesApplication.java
@@ -0,0 +1,13 @@
+package me.ricky.guides.securityguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityGuidesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityGuidesApplication.class, args);
+    }
+
+}
