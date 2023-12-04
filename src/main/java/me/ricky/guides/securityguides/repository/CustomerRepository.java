651b8daed5854507a309a3374acddfeef875e375
RichardKim <hiddeneventor@gmail.com>
2023-12-04 22:05:03 +0900

    Customer Entity, Repository 생성

---
+++ src/main/java/me/ricky/guides/securityguides/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package me.ricky.guides.securityguides.repository;
+
+import me.ricky.guides.securityguides.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findByEmail(String email);
+}
