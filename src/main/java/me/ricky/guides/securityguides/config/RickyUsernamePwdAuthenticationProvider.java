7ffc1e62b5c254b0a40493dd62d2e665e3ea141c
RichardKim <hiddeneventor@gmail.com>
2023-12-05 23:28:51 +0900

    [AuthenticationProvider] 자체 인증 로직 구현 - DaoAuthenticationProvider.authenticate() 기본 인증 안씀 - loadUserByUsername 삭제 (DaoAuthenticationProvider 안쓰기 때문에)

---
+++ src/main/java/me/ricky/guides/securityguides/config/RickyUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,52 @@
+package me.ricky.guides.securityguides.config;
+
+import me.ricky.guides.securityguides.model.Customer;
+import me.ricky.guides.securityguides.repository.CustomerRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class RickyUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public RickyUsernamePwdAuthenticationProvider(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+        this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        // 자체 인증 로직 구현
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customer = customerRepository.findByEmail(username);
+        if (!customer.isEmpty()) {
+            if (passwordEncoder.matches(pwd, customer.get(0).getPwd())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid password!");
+            }
+        } else {
+            throw new BadCredentialsException("No user registered with this details!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        //DaoAuthenticationProvider 클래스의 supports 메소드를 참고하여 구현
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
