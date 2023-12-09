abb2bcb55dd213bf755a447da4f1bccb53ad650d
RichardKim <hiddeneventor@gmail.com>
2023-12-09 22:42:45 +0900

    권한명 수정 SQL

---
+++ src/main/resources/db/migration/common/V4__update_authorities.sql
@@ -0,0 +1,7 @@
+DELETE FROM `authorities`;
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+ VALUES (1, 'ROLE_USER');
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+ VALUES (1, 'ROLE_ADMIN');
