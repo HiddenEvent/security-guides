cb453fff0bf6fef2927fd5b8b28883df15a57990
RichardKim <hiddeneventor@gmail.com>
2023-12-09 23:02:36 +0900

    [Security 필터] 디버그 설정 및 로그 설정 추가

--- src/main/java/me/ricky/guides/securityguides/SecurityGuidesApplication.java
+++ src/main/java/me/ricky/guides/securityguides/SecurityGuidesApplication.java
@@ -2,8 +2,10 @@ package me.ricky.guides.securityguides;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class SecurityGuidesApplication {
 
     public static void main(String[] args) {
