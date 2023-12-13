991c011deff53b38384b1643970e5bc2f16e7a35
RichardKim <hiddeneventor@gmail.com>
2023-12-13 23:16:50 +0900

    [keycloak] 기존 jwt인증 방식 삭제 및 oaut2 resource server 의존 추가 - 자체 인증로직 삭제 - jwt 관련 삭제 - Bcrypt 인코더 삭제 - BasicAuthentication 설정 삭제

--- src/main/java/me/ricky/guides/securityguides/config/SecurityConfig.java
+++ src/main/java/me/ricky/guides/securityguides/config/SecurityConfig.java
@@ -1,19 +1,16 @@
 package me.ricky.guides.securityguides.config;
 
-import me.ricky.guides.securityguides.filter.*;
+import me.ricky.guides.securityguides.filter.CsrfCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,10 +18,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -56,12 +49,7 @@ public class SecurityConfig {
             csrfConfigurer.ignoringRequestMatchers("/contact", "/register");
         });
         http.addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
-        http.addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class);
-        http.addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class);
-        http.addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class);
 
-        http.addFilterAfter(new JwtTokenGeneratorFilter(), BasicAuthenticationFilter.class);
-        http.addFilterBefore(new JwtTokenValidatorFilter(), BasicAuthenticationFilter.class);
 
         // authorize
         http.authorizeHttpRequests((requests) -> requests
--- src/main/java/me/ricky/guides/securityguides/constants/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package me.ricky.guides.securityguides.constants;
-
-public interface SecurityConstants {
-
-    public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
-}
