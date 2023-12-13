d8ebe007d9c643096aab4fddbdf71beb681065b0
RichardKim <hiddeneventor@gmail.com>
2023-12-13 23:17:05 +0900

    fix

--- src/main/java/me/ricky/guides/securityguides/controller/LoginController.java
+++ src/main/java/me/ricky/guides/securityguides/controller/LoginController.java
@@ -12,17 +12,14 @@ import java.util.List;
 @RestController
 public class LoginController {
     private final CustomerRepository customerRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    public LoginController(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+    public LoginController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
     public String registerUser(@RequestBody Customer customer) {
         Customer savedCustomer;
-        customer.setPwd(passwordEncoder.encode(customer.getPwd()));
         customer.setCreateDt(LocalDate.now());
         savedCustomer = customerRepository.save(customer);
         return "User registered successfully with id " + savedCustomer.getId();
